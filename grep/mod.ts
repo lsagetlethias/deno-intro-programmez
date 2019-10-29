401c152d7c23b6d9475e5b4ff46048632621f29a
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2019-10-29 02:45:58 +0100

    :rotating_light: fmt

--- grep/mod.ts
+++ grep/mod.ts
@@ -1,12 +1,12 @@
-import * as path from 'https://deno.land/std/fs/path/mod.ts';
-import { green, red, bold } from 'https://deno.land/std/colors/mod.ts';
+import * as path from "https://deno.land/std/fs/path/mod.ts";
+import { green, red, bold } from "https://deno.land/std/colors/mod.ts";
 
 (async () => {
   if (Deno.args.length != 3) {
     if (Deno.args.length > 3) {
-      throw new Error('grep: to much args.');
+      throw new Error("grep: to much args.");
     } else {
-      throw new Error('grep: missing args.');
+      throw new Error("grep: missing args.");
     }
   }
 
@@ -16,14 +16,15 @@ import { green, red, bold } from 'https://deno.land/std/colors/mod.ts';
     const content = await Deno.readFile(path.resolve(Deno.cwd(), filePath));
 
     let lineNumber = 1;
-    for (const line of new TextDecoder().decode(content).split('\n')) {
+    for (const line of new TextDecoder().decode(content).split("\n")) {
       if (line.includes(text)) {
-        console.log(`${green(`(${lineNumber})`)} ${line.replace(text, red(bold(text)))}`);
+        console.log(
+          `${green(`(${lineNumber})`)} ${line.replace(text, red(bold(text)))}`
+        );
       }
       lineNumber++;
     }
-
   } catch (error) {
-    console.error(`grep: error during process.\n${error}`)
+    console.error(`grep: error during process.\n${error}`);
   }
 })();
